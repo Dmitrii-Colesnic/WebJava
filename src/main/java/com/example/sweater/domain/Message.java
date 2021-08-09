b0893ab68217ab162aa5d56275de328471c05e2a
Colesnic Dmitrii <dmitrii.colesnic.01@gmail.com>
2021-08-09 10:41:40 +0300

    PasswordEncoder was made, registration and login pages were upgrade(some last commits was mistaken)!

---
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -0,0 +1,45 @@
+package com.example.sweater.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Message {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Please fill the message")
+    @Length(max = 2048, message = "Message too long (more then 2048 chars)")
+    private String text;
+
+    @NotBlank(message = "Please fill the TAG")
+    @Length(max = 255, message = "Message too long (more then 255 chars)")
+    private String tag;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    private String filename;
+
+    public Message(String text, String tag, User user) {
+        this.text = text;
+        this.tag = tag;
+        this.author = user;
+    }
+
+    public String getAuthorName() {
+        if (author == null || author.getUsername() == "")
+            return "<none>";
+        else
+            return author.getUsername();
+//        return author != null ? author.getUsername() : "<none>";
+    }
+}
